e6ff1d5cd490f1cb7ebe1ce939d8edddec680c2b
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 11:31:22 +0900

    Update Auction.java

--- Practice/241008/Auction.java
+++ Practice/241008/Auction.java
@@ -1,3 +1,4 @@
+// 금액 비교를 위한 클래스 생성
 class Person implements Comparable<Person> {
     int bal;
     int idx;
@@ -15,8 +16,11 @@ class Person implements Comparable<Person> {
 
 public class MyClass {
     public static void main(String[] args) {
+        // 1번 테스트케이스
         int[] arr1 = { 1000000, 490000, 700000, 290000 };
+        // 2번 테스트케이스
         int[] arr2 = { 30000, 70000, 10000 };
+        
         System.out.println(Arrays.toString(solution(4, arr1)));
         System.out.println(Arrays.toString(solution(6, arr2)));
     }
@@ -30,6 +34,7 @@ public class MyClass {
             p[i] = new Person(amounts[i], i);
         }
 
+        // 배열 정렬 및 자본 순서대로 낙찰 후 변동금액 저장
         for (int i = 0; i < n; i++) {
             Arrays.sort(p);
             if (p[1].bal == 0) {
