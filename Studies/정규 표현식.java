a0bf5ff6adbf878b615666530ea80c8865b7b02f
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 14:31:48 +0900

    Create 정규 표현식.java

---
+++ Studies/정규 표현식.java
@@ -0,0 +1,40 @@
+// [] : 선택
+// () : 그룹
+// - : 범위
+// + : 1개 이상
+// * : 0개 이상
+// ? : 0, 1
+// . : 임의의 문자
+// [0-9a-zA-Z] : \w
+// [0-9] : \d
+private static void m1() {
+    String num = "010-1234-5678";
+    String regExp1 = "([01]{3})-(\\d{3,4})-(\\d{4})";
+
+    String email = "test1234@naver.com";
+    String regExp2 = "(\\w+)@(\\w+\\.\\w+)";
+
+    boolean result1 = Pattern.matches(regExp1, num);
+    System.out.println(result1);
+    boolean result2 = Pattern.matches(regExp2, email);
+    System.out.println(result2);
+
+    Pattern p1 = Pattern.compile(regExp1);
+    Pattern p2 = Pattern.compile(regExp2);
+
+    Matcher m1 = p1.matcher(num);
+    Matcher m2 = p2.matcher(email);
+
+    if (m1.find()) {
+        System.out.println(m1.group(0));
+        System.out.println(m1.group(1));
+        System.out.println(m1.group(2));
+        System.out.println(m1.group(3));
+    }
+
+    if (m2.find()) {
+        System.out.println(m2.group(0));
+        System.out.println(m2.group(1));
+        System.out.println(m2.group(2));
+    }
+}
