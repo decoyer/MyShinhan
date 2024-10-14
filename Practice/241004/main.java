6f90a4f143abb3260635f6c7c32faf8a42d7281d
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 11:41:05 +0900

    Update main.java

--- Practice/241004/main.java
+++ Practice/241004/main.java
@@ -1,19 +1,22 @@
 class Member {
-	int[] solution(int[] periods, int[][] payments, int[] estimates) {
-		int[] result = new int[2];
+    int[] solution(int[] periods, int[][] payments, int[] estimates) {
+        int[] result = new int[2];
 
-		for (int i = 0; i < periods.length; i++) {
+        for (int i = 0; i < periods.length; i++) {
             int paid = 0;
+            // 납부 금액 저장
             for (int j = 0; j < payments[i].length; j++) {
                 paid += payments[i][j];
             }
 
+            // 이번달 기준 VIP 확인
             if (check(paid, periods[i])) {
                 paid = paid - payments[i][0] + estimates[i];
                 if (!check(paid, periods[i] + 1))
                     result[1]++;
             }
 
+            // 다음달 기준 VIP 확인
             else {
                 paid = paid - payments[i][0] + estimates[i];
                 if (check(paid, periods[i] + 1))
@@ -24,12 +27,15 @@ class Member {
         return result;
     }
 
-	private boolean check(int paid, int period) {
+    // VIP 조건
+    private boolean check(int paid, int period) {
+        // 5년 가입 시 60만원 납부 필요
         if (period >= 60 && paid >= 600000)
-                return true;
-		
+            return true;
+
+        // 2년 가입 시 90만원 납부 필요
         else if (period >= 24 && paid >= 900000)
-                return true;
+            return true;
 
         return false;
     }
