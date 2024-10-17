167638f4aaca46840bd6719049dcb62a0ff793ed
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-17 09:04:41 +0900

    Create main.java

---
+++ Exercises/241016/main.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String[] args) {
+        String[] numstrs = {"ZASSETE", "S4Z537B", "7_ASZEYB"};
+        String[] words = {"2455373", "425", "373", "378"};
+
+        System.out.println(Arrays.toString(solution(numstrs, words)));
+    }
+
+    private static int[] solution(String[] numstrs, String[] words) {
+        int[] result = new int[words.length];
+        String[] sign = {"O", "()", "I", "Z", "S", "7_", "E", "B", "A", "b", "G", "T", "Y", "E3", "g", "q"};
+
+        return result;
+    }
+}
