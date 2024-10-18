c9677521aa5309e9c5f9ef107b20d46b8a1becb1
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 16:38:01 +0900

    Update Prob5.java

--- Test/Prob5.java
+++ Test/Prob5.java
@@ -3,20 +3,13 @@ public class Prob5 {
 		int[] answer = { 1, 4, 4, 3, 1, 4, 4, 2, 1, 3, 2 };
 		int[] counter = new int[4];
 
+		// 배열 요소 값의 개수 체크하여 배열에 저장
 		for (int i : answer) {
 			switch (i) {
-				case 1:
-					counter[0]++;
-					break;
-				case 2:
-					counter[1]++;
-					break;
-				case 3:
-					counter[2]++;
-					break;
-				case 4:
-					counter[3]++;
-					break;
+				case 1 -> counter[0]++;
+				case 2 -> counter[1]++;
+				case 3 -> counter[2]++;
+				case 4 -> counter[3]++;
 			}
 		}
 
