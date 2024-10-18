cec07216bbafeb3982528f093fd86226bf2b1bb6
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 16:33:28 +0900

    Update Prob2.java

--- Test/Prob2.java
+++ Test/Prob2.java
@@ -14,10 +14,12 @@ public class Prob2 {
 	public static String leftPad(String str, int size, char fillChar) throws IllegalSizeException {
 		StringBuilder sb = new StringBuilder();
 
-		if(size < str.length()) {
+		// size 값이 문자열의 길이보다 작으면 오류 발생
+		if (size < str.length()) {
 			throw new IllegalSizeException("문자열의 길이보다 size가 큽니다.");
 		}
 
+		// 문자열의 길이 - size 값 만큼 fillChar 입력
 		for (int i = 0; i < 10 - str.length(); i++) {
 			sb.append(fillChar);
 		}
