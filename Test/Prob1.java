6f1597ecc060935cf844ce8c82f6083f890903a0
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 16:29:18 +0900

    Update Prob1.java

--- Test/Prob1.java
+++ Test/Prob1.java
@@ -1,22 +1,26 @@
 public class Prob1 {
-	public static void main(String[] args) {
-		String[] array = { "황남기85점", "조성호89점", "한인성88점", "독고정진77점" };
-		printMaxScore(array);
-	}
+    public static void main(String[] args) {
+        String[] array = { "황남기85점", "조성호89점", "한인성88점", "독고정진77점" };
+        printMaxScore(array);
+    }
 
-	private static void printMaxScore(String[] array) {
-		String maxName = "";
+    private static void printMaxScore(String[] array) {
+        String maxName = "";
         int maxScore = -1;
 
         for (String s : array) {
+	    // 문자열 끝의 불필요한 요소 제외
             int idx = s.length() - 2;
 
+	    // 문자 숫자 분리하기 위해 기준점 설정
             while (Character.isDigit(s.charAt(idx - 1)))
                 idx--;
-			
+
+	    // 문자 숫자 분리
             String name = s.substring(0, idx);
             int score = Integer.parseInt(s.substring(idx, s.length() - 1));
 
+	    // 최고점수 탐색
             if (score > maxScore) {
                 maxScore = score;
                 maxName = name;
@@ -24,5 +28,5 @@ public class Prob1 {
         }
 
         System.out.printf("최고점수는 %s님 %d점 입니다.", maxName, maxScore);
-	}
+    }
 }
